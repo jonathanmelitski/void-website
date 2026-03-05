1ba8fa2264feb90da0f3d76bef1c3c8a60f75867
Jonathan Melitski <jonathanmelitski@gmail.com>
2026-03-05 16:22:55 -0500

    Add rest of roster

--- app/roster/page.tsx
+++ app/roster/page.tsx
@@ -4,7 +4,15 @@ import { SamplePlayers } from "@/models/SampleData";
 
 
 export default async function Roster() {
-  const players: Player[] = SamplePlayers
+  const players: Player[] = SamplePlayers.sort((a, b) => {
+    if (a.is_captain && !b.is_captain) {
+      return -1
+    }
+    if (!a.is_captain && b.is_captain) {
+      return 1
+    }
+    return (a.last_name < b.last_name) ? -1 : 1
+  })
 
   return (
     <div className="lg:px-16 not-lg:px-4">
--- models/SampleData.tsx
+++ models/SampleData.tsx
@@ -3,12 +3,235 @@ import { Team } from "./RosterModels"
 
 export const SamplePlayers: Player[] = [
     {
-        "id": "asdf-01",
+        id: "asdf-01",
         first_name: "Jon",
         last_name: "Melitski",
         number: 6,
         team: Team.void,
         is_captain: true,
         is_active: true
-      }
-]
+    },
+    {
+        id: "asdf-03",
+        first_name: "Alan",
+        last_name: "Wu",
+        number: 69,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-04",
+        first_name: "Cole",
+        last_name: "Woodward",
+        number: 5,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-05",
+        first_name: "Conner",
+        last_name: "Meidt",
+        number: 77,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-06",
+        first_name: "Ethan",
+        last_name: "Dong",
+        number: 14,
+        jersey_name_text: "MOST",
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-07",
+        first_name: "Greg",
+        last_name: "Gabelus",
+        number: 22,
+        jersey_name_text: "PRESIDENT",
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-08",
+        first_name: "Haven",
+        last_name: "Lo",
+        number: 0,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-09",
+        first_name: "Leonard",
+        last_name: "Vekker",
+        number: 88,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-10",
+        first_name: "Luke",
+        last_name: "Meyer",
+        number: 9,
+        jersey_name_text: "LEMON",
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-11",
+        first_name: "Luke",
+        last_name: "Oh",
+        number: 3,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-12",
+        first_name: "Mati",
+        last_name: "Okutsu",
+        number: 16,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-13",
+        first_name: "Matthew",
+        last_name: "Seiberlich",
+        number: 26,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-14",
+        first_name: "Max",
+        last_name: "Huang",
+        number: 13,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-15",
+        first_name: "Maxwell",
+        last_name: "Tran",
+        number: 8,
+        team: Team.void,
+        is_captain: true,
+        is_active: true
+    },
+    {
+        id: "asdf-16",
+        first_name: "Nikhil",
+        last_name: "Grobes",
+        number: 2,
+        team: Team.void,
+        is_captain: true,
+        is_active: true
+    },
+    {
+        id: "asdf-17",
+        first_name: "Oliver",
+        last_name: "Sun",
+        number: 15,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-18",
+        first_name: "Ricardo",
+        last_name: "Wang-Polendo",
+        number: 25,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-19",
+        first_name: "Sammy",
+        last_name: "Abusaab",
+        number: 95,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-20",
+        first_name: "Sean",
+        last_name: "Feng",
+        number: 30,
+        jersey_name_text: "冯泳钦",
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-21",
+        first_name: "Stone",
+        last_name: "Xin",
+        number: 1,
+        jersey_name_text: "PEKKA",
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-22",
+        first_name: "Texas",
+        last_name: "Maki",
+        number: 28,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-23",
+        first_name: "Tomas",
+        last_name: "Falleti-Moore",
+        number: 27,
+        jersey_name_text: "FM TT",
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-24",
+        first_name: "Xavier",
+        last_name: "Allen",
+        number: 31,
+        jersey_name_text: "X",
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-25",
+        first_name: "Zulqarnain",
+        last_name: "Sheikh",
+        number: 32,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    },
+    {
+        id: "asdf-27",
+        first_name: "Luke",
+        last_name: "Neale",
+        number: 91,
+        team: Team.void,
+        is_captain: false,
+        is_active: true
+    }
+];
